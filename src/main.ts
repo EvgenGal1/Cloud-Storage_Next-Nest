c1fa8bf55100f8c88ef55d1ae5b14956df1c3392
EvGen Gal <genwolk1@gmail.com>
2023-10-24 02:31:47 +0300

    правки/настройки; !ошб.при подкл.к БД

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+// попытка HMR с webpack
+// declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
 
@@ -19,7 +22,13 @@ async function bootstrap() {
   // меняем путь вместо 'api' - 'swagger'
   SwaggerModule.setup('swagger', app, document);
 
-  // свой порт
-  await app.listen(7531);
+  // ? свой порт сервера ? клиента
+  await app.listen(7531 /* 5125 */);
+
+  // попытка HMR с webpack
+  // if (module.hot) {
+  //   module.hot.accept();
+  //   module.hot.dispose(() => app.close());
+  // }
 }
 bootstrap();
