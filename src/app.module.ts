6328adcea6a8ca50965bad23259f8781352743ee
EvGen Gal <genwolk1@gmail.com>
2023-10-24 23:57:21 +0300

    $ config; # настр.подкл.к БД ч/з process.env;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/user.entity';
+import { FileEntity } from './files/entities/file.entity';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [UsersModule, FilesModule],
+  imports: [
+    // подкл.модуль для счит.из.env
+    ConfigModule.forRoot(),
+    // подкл.к БД ч/з перем.process.env
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      // указ.сущн. для авто.синхронз.измен. ч/з TypeOrmModule
+      entities: [UserEntity, FileEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
