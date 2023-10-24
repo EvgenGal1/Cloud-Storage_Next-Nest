53744424269f9a3efeafdf450d045d4364e17da2
EvGen Gal <genwolk1@gmail.com>
2023-10-24 23:55:20 +0300

    настр.User.File.Entity; Созд.Табл.,связей

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -1,1 +1,34 @@
-export class File {}
+import { UserEntity } from 'src/users/entities/user.entity';
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('files')
+export class FileEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fileName: string;
+
+  @Column()
+  originalName: string;
+
+  @Column()
+  size: number;
+
+  @Column()
+  mimetype: string;
+
+  // связь табл. Мн.к 1му. У Мн.данн.(файлов) Одна привязка (Один польз.)
+  @ManyToOne(() => UserEntity, (user: UserEntity) => user.files)
+  user: UserEntity;
+
+  // декоратор поментки удаления (без удаления)
+  @DeleteDateColumn()
+  daletedAt?: Date;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,2 +1,37 @@
-// сущн.взаимод.с БД (стркт.табл./измен.данн.в табл.User)
-export class User {}
+// ^ сущн.взаимод.с БД (стркт.табл./измен.данн.в табл.User)
+
+// декораторы для раб.с БД
+import { FileEntity } from 'src/files/entities/file.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+// декоратор для соед.с БД
+@Entity('users')
+export class UserEntity {
+  // декоратор для авто.генер.id
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // созд.простых колонок
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  fullName: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  activated: boolean;
+
+  @Column()
+  link: string;
+
+  // связь табл. 1го ко Мн. 1ый аргум.аноним.fn (табл.обращения - FileEntity), 2ый парам.получ.данн.и обратн.связь
+  @OneToMany(() => FileEntity, (file: FileEntity) => file.user)
+  // типиз.данн. (возвращ.список FileEntity)
+  files: FileEntity[];
+}
