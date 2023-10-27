595a2b981f0daf51e2357a561edaa17fe8d519a0
EvGen Gal <genwolk1@gmail.com>
2023-10-27 10:52:20 +0300

    в мтд.авториз.указ.переданный объ: доп.в cre-u.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 // разрещ.данн.res front > dack. Отдел.кл.с опис.ожид.св-в
 export class CreateUserDto {
+  // доп.декор для поним.swagger какие есть св-ва. Можно указ.знач.по умолч. в {default:''}
+  @ApiProperty({
+    default: 'test@test.ru',
+  })
   email: string;
+
+  @ApiProperty({
+    default: 'Тест Тестович',
+  })
   fullName: string;
+
+  @ApiProperty({
+    default: '123_Тест',
+  })
   password: string;
 }
