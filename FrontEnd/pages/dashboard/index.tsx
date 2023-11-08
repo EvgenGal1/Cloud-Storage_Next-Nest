07657e8b28b39724ee930a859155ecddf139f952
EvGen Gal <genwolk1@gmail.com>
2023-11-08 03:18:34 +0300

    #pages/dashbrd/idx.tsx (загр.ф., отрис.список)

--- FrontEnd/pages/dashboard/index.tsx
+++ FrontEnd/pages/dashboard/index.tsx
@@ -1,31 +1,52 @@
-// страница `панель приборов`
+// ^ Главн.Стр./DashboardPage(стр.`панель приборов`)
 import { GetServerSidePropsContext, NextPage } from 'next';
 import React from 'react';
 // import axios from 'axios';
 // import nookies from 'nookies';
 
-// import * as Api from '@/api';
+import * as Api from '@/api';
 import { FileItem } from '@/api/dto/files.dto';
 import { Layout } from '@/layouts/Layout';
-// import { DashboardLayout } from '@/layouts/DashboardLayout';
-// import { checkAuth } from "@/utils/checkAuth";
+import { DashboardLayout } from '@/layouts/DashboardLayout';
 import { checkAuth } from '@/utils/checkAuth';
-// import { Files } from "@/modules/Files";
-import { Header } from '@/components/Header';
+// import { Files } from '@/modules/Files';
+import { FileList } from '@/components/FileList';
+// import { Header } from '@/components/Header';
 
+// import { UploadButton } from '@/components/UploadButton';
+import { useRouter } from 'next/router';
+// import { Menu } from 'antd';
+// import {
+//   DeleteOutlined,
+//   FileImageOutlined,
+//   FileOutlined,
+// } from '@ant-design/icons';
+
+// import styles from '@/styles/Home.module.scss';
+
+// приним.масс.ф.с БД
 interface Props {
   items: FileItem[];
 }
 
-const DashboardPage: NextPage /* <Props> */ = (/* { items } */) => {
+// приним.масс.ф.с БД
+// const DashboardPage: NextPage<Props> = ({ items }) => {
+// !! Свойство "getLayout" не существует в типе "FunctionComponent<{}> & ...
+// ~~ вар.решения убрать тип для const и постав.тип для парам.
+const DashboardPage = ({ items }: any) => {
+  const router = useRouter();
+  const selectedMenu = router.pathname;
+
   return (
-    <main>
-      {/* <Header /> // откл. > getLayout */}
-      <div>Панель инструментов</div>
-    </main>
-    // <DashboardLayout>
-    //   <Files items={items} withActions />
-    // </DashboardLayout>
+    // откл. > DashboardLayout
+    // <main>
+    //   {/* <Header /> // откл. > getLayout */}
+    //   <div>Панель инструментов</div>
+    // </main>
+    <DashboardLayout>
+      {/* <Files items={items} withActions /> */}
+      <FileList items={items} />
+    </DashboardLayout>
   );
 };
 
@@ -45,10 +66,28 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     // отправ.на Авториз.
     return authProps;
   }
-  return {
-    // е/и redirect нет возвращ.пуст.props(остаёмся на "/dashboard")
-    props: {},
-  };
+
+  // упразд. > DashboardLayout.FileList
+  // е/и redirect нет возвращ.пуст.props(остаёмся на "/dashboard")
+  // return {
+  //   props: {},
+  // };
+
+  // req список файлов с БД
+  try {
+    const items = await Api.files.getAll();
+    console.log('items ', items);
+    return {
+      props: {
+        items,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      props: { items: [] },
+    };
+  }
 };
 
 export default DashboardPage;
